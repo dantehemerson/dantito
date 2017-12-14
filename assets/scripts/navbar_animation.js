19f45af9195db0aacfc44fec2e9a0ca20ccc9599
dantehemerson <dantehemerson@gmail.com>
2017-12-14 11:30:08 -0500

    Cambio del background

--- assets/scripts/navbar_animation.js
+++ assets/scripts/navbar_animation.js
@@ -1,24 +1,22 @@
+$('.navbar-custom')
+	.addClass('shrink');
 
-
-	$('.navbar-custom')
-		.addClass('shrink');
-
-	// Animación de la navbar cuando se hace scroll 
-	// en el index.html solo.
-	$(function() {	
-		$(window).scroll(function() {
-			$('.navbar-custom').css('transition', '0.5s');
-			var scroll = getCurrentScroll();
-			if(scroll >= 100) {
-				$('.navbar-custom').removeClass('shrink');
-			}
-			else {
-				$('.navbar-custom').addClass('shrink');
-			} 
-
-		});
-
-		function getCurrentScroll()  {
-			return window.pageYOffset || document.documentElement.scrollTop;
+// Animación de la navbar cuando se hace scroll 
+// en el index.html solo.
+$(function() {	
+	$(window).scroll(function() {
+		$('.navbar-custom').css('transition', '0.5s');
+		var scroll = getCurrentScroll();
+		if(scroll >= 100) {
+			$('.navbar-custom').removeClass('shrink');
 		}
+		else {
+			$('.navbar-custom').addClass('shrink');
+		} 
+
 	});
+
+	function getCurrentScroll()  {
+		return window.pageYOffset || document.documentElement.scrollTop;
+	}
+});
