4e824b364e39f5bff79f467f27b8b38c0cdab18b
dh <dh@x.dh.com>
2018-04-20 21:36:58 -0500

    Añadiendo disqus

---
+++ assets/scripts/disqus.js
@@ -0,0 +1,27 @@
+
+var disqus_config = function () {
+
+	var pathname = document.location.pathname;
+	var dividerList = pathname.split("/");
+	if(dividerList[dividerList.length - 1] == "") {			   
+	   pathname = pathname.slice(0, -1)
+	}			      
+    pathname = pathname.replace(".html", "");
+    
+	this.page.url = document.location.href;  
+	this.page.identifier = pathname;
+
+	console.log("URL : " + this.page.url);
+	console.log("ID  : " + this.page.identifier);
+};
+
+(function() { 
+	var d = document;
+	var s = d.createElement('script');
+	s.src = 'https://dantecalderon.disqus.com/embed.js';
+	s.setAttribute('data-timestamp', +new Date());
+	(d.head || d.body).appendChild(s);
+})();
+
+                            
+
