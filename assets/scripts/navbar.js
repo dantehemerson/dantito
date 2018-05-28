154e88add150db6a1948fcae101a7e2a1b4b253e
Dante Calderón <dantehemerson@gmail.com>
2018-05-28 07:44:27 -0500

    Hide show on scroll

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -3,9 +3,18 @@ var didScroll;
 var lastScrollTop = 0;
 var delta = 5;
 var navbarHeight = $('nav').outerHeight();
+var scroll;
 
 
 $(window).scroll(function(event){
+    var st = $(this).scrollTop();
+   
+    if(st >= 70) {
+        $('nav').addClass('no-topbar');
+    }else {
+        $('nav').removeClass('no-topbar').removeClass('nav-up');
+    }
+
     didScroll = true;
 });
 
@@ -14,24 +23,30 @@ setInterval(function() {
         hasScrolled();
         didScroll = false;
     }
-}, 1);
+}, 250);
+
+
+
 
 function hasScrolled() {
     var st = $(this).scrollTop();
     
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('nav').addClass('nav-up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('nav').removeClass('nav-up');
+
+    if(st >= 200) {        
+        // Make sure they scroll more than delta
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+        
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight){
+            // Scroll Down
+            $('nav').addClass('nav-up');
+        } else {
+            // Scroll Up
+            if(st + $(window).height() < $(document).height()) {
+                $('nav').removeClass('nav-up');
+            }
         }
     }
     
