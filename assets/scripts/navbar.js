c2e61bdf729103c235ed93757261e3ae98f9e7a6
Dante Calderón <dantehemerson@gmail.com>
2018-06-28 05:17:47 -0500

    ...2

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -391,36 +391,32 @@ Headroom.options = {
 };
 Headroom.cutsTheMustard = typeof features !== 'undefined' && features.rAF && features.bind && features.classList;
 
-
-
-
-
 var stateManager = (function () {  
     let state = null;
     var resizePage = function () {
         if (window.innerWidth < 576) {           
-           $('#navbarNav').addClass('xs'); 
-           if(state !== "mobile") { 
-                // Entra por por primera vez en mobile
-                state = "mobile";        
+           // $('#navbarNav').addClass('xs'); 
+           // if(state !== "mobile") { 
+           //      // Entra por por primera vez en mobile
+           //      state = "mobile";        
 
-           }                                 
+           // }                                 
         }
         else {
-           $('nav').removeClass('navbar-open');           
-           $('#navbarNav').removeClass('xs');  
-            $('#navbarNav').addClass('hide'); 
-           if(state !== "no-mobile") {
-                // Ya no esta en monbile
-                state = "no-mobile";
-           }
+           // $('nav').removeClass('navbar-open');           
+           // $('#navbarNav').removeClass('xs');  
+           //  $('#navbarNav').addClass('hide'); 
+           // if(state !== "no-mobile") {
+           //      // Ya no esta en monbile
+           //      state = "no-mobile";
+           // }
         }
     }; 
 
   return {
     init: function () {
-      resizePage();
-      $(window).on('resize', resizePage);
+      // resizePage();
+      // $(window).on('resize', resizePage);
     }
   };
 } ());
@@ -428,25 +424,6 @@ stateManager.init();
 
 
 let navbarNav = $('#navbarNav');
-var navbarHeight = $('nav').outerHeight();
-
-$('#navbarToggler').on('click', function() {    
-    
-
-    $('nav').toggleClass('navbar-open');
-    
-    if($('nav').hasClass('navbar-open')) {
-        navbarNav.removeClass('hide');        
-       // $('#mainWrapper').addClass('blur');
-    }
-    else {
-      //  $('#mainWrapper').removeClass('blur');
-        setTimeout(function() {
-            navbarNav.addClass('hide');
-        }, 400);
-    }
-});
-
 
 $(window).scroll(function(event){
     hasScrolled();   
