c4ad1adfe01a2ea47a89f697272dccb1dde07f8d
dantehemerson <dantehemerson@gmail.com>
2018-11-19 17:00:19 -0500

    Change disqus site

--- assets/scripts/disqus.js
+++ assets/scripts/disqus.js
@@ -18,7 +18,7 @@ var disqus_config = function () {
 (function() { 
 	var d = document;
 	var s = d.createElement('script');
-	s.src = 'https://dantecalderon.disqus.com/embed.js';
+	s.src = 'https://dantito.disqus.com/embed.js';
 	s.setAttribute('data-timestamp', +new Date());
 	(d.head || d.body).appendChild(s);
 })();
